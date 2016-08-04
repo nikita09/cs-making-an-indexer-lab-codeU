dda04cb3d699f5550653feb9e7f0a4f1fc89c6a5
Nikita Zamwar <nikitazamwar@D-10-157-74-8.dhcp4.washington.edu>
2016-08-03 23:05:13 -0700

    Done.

--- javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
+++ javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
@@ -37,7 +37,13 @@ public class TermCounter {
 	 */
 	public int size() {
         // TODO: fill this in.
-		return -1;
+        int size = 0;
+
+        for (String key: keySet()) {
+			size += get(key);
+		}
+		
+		return size;
 	}
 
 	/**
